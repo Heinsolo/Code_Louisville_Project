ee97a28ea10e6578d79174f679eaddcc11988cdf
jacob <jacob.hein77@gmail.com>
2018-06-25 20:08:00 -0400

    Added a whole bunch of stuff

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 This is the README file for the Code Louisville Project for the May 2018 Class.
 
-As of June 4th, not much has changed. I'm still working on my code. duh
+The password is 'Rowdy'
+
+Please have fun and put in a different password if you want to execute the remainder of the JavaScript!
---
+++ script.js
@@ -0,0 +1,8 @@
+var password = prompt("What is the password?")
+  if (password === 'Rowdy') {
+    alert("That is correct. Please enter.");
+    }
+  else {
+    alert('That is incorrect. Please press OK.');
+    window.location.replace('http://i0.kym-cdn.com/photos/images/original/000/237/664/f8b.gif');
+  }
