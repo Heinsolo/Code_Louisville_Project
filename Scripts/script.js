1b5b1d5ff7785dcaa32f469a85aa5fba31cff4ef
jacob <jacob.hein77@gmail.com>
2018-07-02 19:52:34 -0400

    just need JS left

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,3 +6,5 @@ var password = prompt("What is the password?")
     alert('That is incorrect. Please press OK.');
     window.location.replace('http://i0.kym-cdn.com/photos/images/original/000/237/664/f8b.gif');
   }
+
+$('.collapse').collapse()
