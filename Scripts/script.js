076572a66513fa36b3aa3bdfb117ed61238f4423
jacob <jacob.hein77@gmail.com>
2018-07-02 20:05:54 -0400

    test

--- Scripts/script.js
+++ Scripts/script.js
@@ -6,5 +6,3 @@ var password = prompt("What is the password?")
     alert('That is incorrect. Please press OK.');
     window.location.replace('http://i0.kym-cdn.com/photos/images/original/000/237/664/f8b.gif');
   }
-
-$('.collapse').collapse()
