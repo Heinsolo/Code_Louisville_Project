4337084f2fe85e4f0d888d681c1a9e8d190520bf
jacob <jacob.hein77@gmail.com>
2018-07-16 19:16:53 -0400

    Final!

--- Scripts/jquery.js
+++ Scripts/jquery.js
@@ -6,3 +6,12 @@ function showpopup() {
   $(".form").fadeIn();
   $(".button").hide();
 }
+
+$(".cancel" ).click(function() {
+  hidepopup();
+});
+
+function hidepopup() {
+  $(".form").fadeOut();
+  $(".button").show();
+}
